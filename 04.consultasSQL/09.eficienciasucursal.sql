aecbb8add7cf3068eef771d927656868037708a6
Oliver Morales <oliverjmp@gmail.com>
2025-09-07 14:35:05 +0200

    actualizacion 07/09/2025 14:33

--- 04.consultasSQL/09.eficienciasucursal.sql
+++ 04.consultasSQL/09.eficienciasucursal.sql
@@ -1,12 +1,17 @@
 USE [DatacrafterDB];
 GO
-SELECT s.branch_id, s.country,
-       COUNT(DISTINCT v.purchase_id) AS ventas,
-       SUM(dv.cantidad * dv.precio_unitario) AS ingresos,
-       COUNT(DISTINCT d.devolucion_id) AS devoluciones,
-       CAST(COUNT(DISTINCT d.devolucion_id) AS FLOAT) / NULLIF(COUNT(DISTINCT v.purchase_id), 0) AS tasa_devolucion
+
+SELECT 
+    s.branch_id,
+    s.country,
+    s.city,
+    COUNT(DISTINCT v.purchase_id) AS ventas,
+    SUM(dv.cantidad * dv.precio_unitario) AS ingresos,
+    COUNT(DISTINCT d.devolucion_id) AS devoluciones,
+    CAST(COUNT(DISTINCT d.devolucion_id) AS FLOAT) / NULLIF(COUNT(DISTINCT v.purchase_id), 0) AS tasa_devolucion
 FROM sucursales s
 LEFT JOIN ventas v ON s.branch_id = v.branch_id
 LEFT JOIN detalle_ventas dv ON v.purchase_id = dv.purchase_id
 LEFT JOIN devoluciones d ON v.purchase_id = d.venta_id
-GROUP BY s.branch_id, s.country;
+GROUP BY s.branch_id, s.country, s.city;
+
